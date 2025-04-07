73d494ca9c2e05168522fbc4a37806fb85a082fd
LVS-Garbinho <lucasvalsiq.1203@gmail.com>
2025-04-07 10:55:03 -0300

    Criação do Projeto TempoAgora

--- MauiAppTempoAgora/MainPage.xaml.cs
+++ MauiAppTempoAgora/MainPage.xaml.cs
@@ -1,24 +1,51 @@
-﻿namespace MauiAppTempoAgora
+﻿using MauiAppTempoAgora.Models;
+using MauiAppTempoAgora.Services;
+
+namespace MauiAppTempoAgora
 {
     public partial class MainPage : ContentPage
     {
-        int count = 0;
-
         public MainPage()
         {
             InitializeComponent();
         }
 
-        private void OnCounterClicked(object sender, EventArgs e)
+        public async void Button_Clicked(object sender, EventArgs e)
         {
-            count++;
+            try
+            { 
+                if(!string.IsNullOrEmpty(txt_cidade.Text))
+                {
+                    Tempo? t = await DataService.GetPrevisao(txt_cidade.Text);
 
-            if (count == 1)
-                CounterBtn.Text = $"Clicked {count} time";
-            else
-                CounterBtn.Text = $"Clicked {count} times";
+                    if (t != null)
+                    {
+                        string dados_previsao = "";
 
-            SemanticScreenReader.Announce(CounterBtn.Text);
+                        dados_previsao = $"Latitude:{t.lat} \n" +
+                                         $"Longitude:{t.lon} \n" +
+                                         $"Nascer do sol:{t.sunrise} \n" +
+                                         $"Por do sol:{t.sunset} \n" +
+                                         $"Temp Máx:{t.temp_max} \n" +
+                                         $"Temp Min:{t.temp_min} \n" +
+                                         $"Como está o clima:{t.description} \n" +
+                                         $"Velocidade do vento:{t.speed} \n" +
+                                         $"Visibilidade:{t.visibility} \n";
+
+                    } else
+                    {
+                        lbl_res.Text = "A previsão não foi encontrada. Por favor, verifique se o nome da cidade foi digitado corretamente.";
+                    }
+                   
+                } else
+                {
+                    lbl_res.Text = "Preencha a Cidade.";
+                }
+
+            } catch(Exception ex)
+            {
+                await DisplayAlert("Ops", ex.Message, "OK");
+            }
         }
     }
 
